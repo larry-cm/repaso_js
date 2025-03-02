80d89fcf2dade61169f9fa7bcaa01d5d1165b399
larry-cm <xetisimo@gmail.com>
2025-03-02 12:23:20 -0500

    update-level

---
+++ fundamentos_avanzados/Readme.md
@@ -0,0 +1,14 @@
+# Vamos a fortalecer las bases que tengo avanzadas de js
+
+en este modulo veremos cosas como promesas, async, await, request, sobre erres y como manejarlos.
+
+## Las clases son plantillas de los objetos
+
+- Las clases son plantillas para objetos.
+- JavaScript llama a un método constructor cuando creamos una nueva instancia de una clase.
+- La herencia es cuando creamos una clase padre con propiedades y métodos que podemos extender a clases hijas.
+- Usamos la palabra clave extends para crear una subclase.
+- La palabra clave super llama al constructor() de una clase padre.
+- Los métodos estáticos se llaman en la clase, pero no en instancias de la clase.
+
+## 
---
+++ fundamentos_avanzados/clases.js
@@ -0,0 +1,40 @@
+// clase de repaso numero 1
+
+class HospitalEmployee {
+    constructor(name) {
+        this._name = name;
+        this._remainingVacationDays = 20;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get remainingVacationDays() {
+        return this._remainingVacationDays;
+    }
+
+    set takeVacationDays(daysOff) {
+        this._remainingVacationDays -= daysOff;
+    }
+
+    static generatePassword() {
+        return Math.floor(Math.random() * 10000);
+    }
+}
+
+// subclase de repaso numero 2
+class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+        super(name);
+        this._certifications = certifications;
+    }
+
+    get certifications() {
+        return this._certifications;
+    }
+
+    set addCertification(newCertification) {
+        this.certifications.push(newCertification);
+    }
+}
